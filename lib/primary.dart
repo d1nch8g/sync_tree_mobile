a6bf02d1f7b3d8d42ccec90f58f2fb6c940ec02a
Danila Fomin <dangdancheg@gmail.com>
2021-06-10 00:56:08 +0300

    .

---
+++ lib/primary.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'market/main.dart';
+import 'wallet/main.dart';
+import 'settings/main.dart';
+import 'balance/main.dart';
+import 'start/main.dart';
+
+
+
+class PrimaryPage extends StatefulWidget {
+  @override
+  _PrimaryPageState createState() => _PrimaryPageState();
+}
+
+class _PrimaryPageState extends State<PrimaryPage> {
+  int _selectedIndex = 0;
+  PageController _pageController;
+
+  Future<bool> firstLaunch() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var firstLaunch = prefs.getBool('firstLaunch');
+    prefs.setBool('firstLaunch', false);
+    return firstLaunch ?? true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    firstLaunch().then(
+      (firstLaunch) => {
+        if (firstLaunch) {Navigator.pushNamed(context, '/firstLaunch')}
+      },
+    );
+    return Scaffold(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _selectedIndex = index);
+          },
+          children: <Widget>[
+            MarketPage(),
+            BalancePage(),
+            WalletPage(),
+            SettingsPage(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Theme.of(context).focusColor,
+        unselectedItemColor: Theme.of(context).focusColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'markets',
+            icon: Icon(Icons.insert_chart_outlined_sharp),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: 'balance',
+            icon: Icon(Icons.autorenew_sharp),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: 'wallet',
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            label: 'settings',
+            icon: Icon(Icons.settings),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.animateToPage(index,
+          duration: Duration(milliseconds: 320), curve: Curves.easeOut);
+    });
+  }
+}
--- lib/start/main.dart
+++ lib/start/main.dart
@@ -10,7 +10,7 @@ class IntroPage extends StatelessWidget {
           child: TextButton(
             child: Text('hello'),
             onPressed: () {
-              Navigator.pushNamed(context, '/app');
+              Navigator.pushNamed(context, '/');
             },
           ),
         ),
