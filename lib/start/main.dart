9eed55e5439dd1d618def210a23254beea42c03a
Danila Fomin <dangdancheg@gmail.com>
2021-06-10 00:44:38 +0300

    .

--- lib/start/main.dart
+++ lib/start/main.dart
@@ -2,16 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class IntroPage extends StatelessWidget {
-  Future<bool> navigateToMain(context) async {
+  Future<bool> firstLaunch() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    var firstLaunch = prefs.getBool('notFirstLaunch');
-    if (firstLaunch) {
-      Navigator.pop(context);
-    }
+    var firstLaunch = prefs.getBool('firstLaunch');
+    prefs.setBool('firstLaunch', false);
+    print(firstLaunch);
+    return firstLaunch ?? true;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    firstLaunch().then(
+      (firstLaunch) => {
+        if (firstLaunch) {Navigator.pushNamed(context, '/app')},
+      },
+    );
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: TextButton(
+            child: Text('hello'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/app');
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
