0784d5043da7465be75603c48a047905421fc4c2
Danila Fomin <dangdancheg@gmail.com>
2021-06-16 19:15:43 +0300

    .

--- lib/settings/copyPriv.dart
+++ lib/settings/copyPriv.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void copyPrivateKey() {
+void copyPrivateKey(context) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
   
+  var key = prefs.getString('persPriv');
+  showDialog(
+    context: context,
+    builder: (_) => ButtonOverlay(
+      () {
+        Navigator.pushNamed(context, '/main');
+      },
+      messageText: 'Key is copied to\nclipboard. Save it\n in safe place!',
+      buttonText: 'continue',
+    ),
+  );
 }
