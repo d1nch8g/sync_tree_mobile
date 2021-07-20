2fd878273a7494d6c8b0bd5cdfbcba54b1e478cb
Danila Fomin <dangdancheg@gmail.com>
2021-07-20 09:40:30 +0300

    fixed message to cancel user trades

--- lib/_calls/cancelTrade.dart
+++ lib/_calls/cancelTrade.dart
@@ -1,23 +1,24 @@
-import 'dart:convert';
 import 'dart:typed_data';
+
 import 'package:grpc/grpc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../crypt.dart';
-import '../api/api.pb.dart';
-import '../api/api.pbgrpc.dart';
-import '../api/api.dart';
+
+import '../_local/keys.dart';
+import '../_local/crypt.dart';
+import '../_api/api.pb.dart';
+import '../_api/api.pbgrpc.dart';
+import '../_api/api.dart';
 
 Future<bool> userCancelTrade(Uint8List marketAdress) async {
   try {
-    var crypt = Crypt();
-    var keys = await crypt.getAllKeys();
-    var persPub = crypt.keyToBytes(keys[Key.PersonalPublicKey]!);
+    var keys = await getAllKeys();
+    var persPub = keyToBytes(keys[Key.PersonalPublicKey]!);
     var concatmessage1 = Uint8List.fromList(
       []..addAll(persPub)..addAll(marketAdress),
     );
     var prefs = await SharedPreferences.getInstance();
     var persPrivString = prefs.getString('persPriv') ?? '';
-    var sign = crypt.signMessage(persPrivString, concatmessage1);
+    var sign = signMessage(persPrivString, concatmessage1);
     final response = await stub.userCancelTrade(
       UserCancelTradeRequest(
         publicKey: persPub,
