6f8aaf0ccada3d93998ebea5e9ada0eeba775cda
Danila Fomin <dangdancheg@gmail.com>
2021-08-13 15:43:15 +0300

    changes in update function

--- lib/_calls/send.dart
+++ lib/_calls/send.dart
@@ -11,30 +11,28 @@ import '../_api/api.pb.dart';
 import '../_api/api.pbgrpc.dart';
 import '../_api/api.dart';
 
-Future<bool> userSend(String recieverAdressBase64, Int64 amount) async {
-  try {
-    var prefs = await SharedPreferences.getInstance();
-    var persPubString = prefs.getString('persPub') ?? '';
-    var persPub = keyToBytes(persPubString);
-    var recieverAdress = base64.decode(recieverAdressBase64);
-    var amountAsBytes = amount.toBytes();
-    var concatmessage1 = Uint8List.fromList(
-        []..addAll(persPub)..addAll(amountAsBytes)..addAll(recieverAdress));
-    var persPrivString = prefs.getString('persPriv') ?? '';
-    var sign = signMessage(persPrivString, concatmessage1);
-    final response = await stub.userSend(
-      UserSendRequest(
-        publicKey: persPub,
-        sendAmount: amount,
-        recieverAdress: recieverAdress,
-        sign: sign,
-      ),
-      options: CallOptions(
-        timeout: Duration(milliseconds: 2584),
-      ),
-    );
-    return response.passed;
-  } catch (Exception) {
-    return false;
-  }
+Future<bool> userSend(
+    context, String recieverAdressBase64, Int64 amount) async {
+  var prefs = await SharedPreferences.getInstance();
+  var persPubString = prefs.getString('persPub') ?? '';
+  var persPub = keyToBytes(persPubString);
+  var recieverAdress = base64.decode(recieverAdressBase64);
+  var amountAsBytes = amount.toBytes();
+  var concatmessage1 = Uint8List.fromList(
+      []..addAll(persPub)..addAll(amountAsBytes)..addAll(recieverAdress));
+  var persPrivString = prefs.getString('persPriv') ?? '';
+  var sign = signMessage(persPrivString, concatmessage1);
+  var stub = getStub(context);
+  final response = await stub.userSend(
+    UserSendRequest(
+      publicKey: persPub,
+      sendAmount: amount,
+      recieverAdress: recieverAdress,
+      sign: sign,
+    ),
+    options: CallOptions(
+      timeout: Duration(milliseconds: 2584),
+    ),
+  );
+  return response.passed;
 }
--- lib/_calls/update.dart
+++ lib/_calls/update.dart
@@ -3,39 +3,36 @@ import 'dart:typed_data';
 
 import 'package:grpc/grpc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sync_tree_mobile/_api/api.dart';
 
 import '../_local/crypt.dart';
 import '../_local/keys.dart';
-import '../_api/api.dart';
 import '../_api/api.pb.dart';
 import '../_api/api.pbgrpc.dart';
 
-Future<bool> userUpdate() async {
-  try {
-    var prefs = await SharedPreferences.getInstance();
-    var persPubString = prefs.getString('persPub') ?? '';
-    var persPub = keyToBytes(persPubString);
-    var pubName = prefs.getString('pubName') ?? '';
-    var pubNameBytes = utf8.encode(pubName);
-    var mesPubString = prefs.getString('mesPub') ?? '';
-    var mesPub = keyToBytes(mesPubString);
-    var concatmessage1 = Uint8List.fromList(
-        []..addAll(persPub)..addAll(mesPub)..addAll(pubNameBytes));
-    var persPrivString = prefs.getString('persPriv') ?? '';
-    var sign = signMessage(persPrivString, concatmessage1);
-    final response = await stub.userUpdate(
-      UserUpdateRequest(
-        publicKey: persPub,
-        messsageKey: mesPub,
-        publicName: pubName,
-        sign: sign,
-      ),
-      options: CallOptions(
-        timeout: Duration(milliseconds: 2584),
-      ),
-    );
-    return response.passed;
-  } catch (Exception) {
-    return false;
-  }
+Future<bool> userUpdate(context) async {
+  var prefs = await SharedPreferences.getInstance();
+  var persPubString = prefs.getString('persPub') ?? '';
+  var persPub = keyToBytes(persPubString);
+  var pubName = prefs.getString('pubName') ?? '';
+  var pubNameBytes = utf8.encode(pubName);
+  var mesPubString = prefs.getString('mesPub') ?? '';
+  var mesPub = keyToBytes(mesPubString);
+  var concatmessage1 = Uint8List.fromList(
+      []..addAll(persPub)..addAll(mesPub)..addAll(pubNameBytes));
+  var persPrivString = prefs.getString('persPriv') ?? '';
+  var sign = signMessage(persPrivString, concatmessage1);
+  var stub = getStub(context);
+  final response = await stub.userUpdate(
+    UserUpdateRequest(
+      publicKey: persPub,
+      messsageKey: mesPub,
+      publicName: pubName,
+      sign: sign,
+    ),
+    options: CallOptions(
+      timeout: Duration(milliseconds: 2584),
+    ),
+  );
+  return response.passed;
 }
