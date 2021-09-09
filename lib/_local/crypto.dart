d604ff8a82a36f343adb2a02b5e38e17d9ad5c2d
Danila Fomin <dangdancheg@gmail.com>
2021-09-09 14:09:36 +0300

    .

--- lib/_local/crypto.dart
+++ lib/_local/crypto.dart
@@ -3,23 +3,52 @@ import 'dart:typed_data';
 
 import 'package:pointycastle/pointycastle.dart';
 import 'package:basic_utils/basic_utils.dart';
+import 'package:sync_tree_mobile/_local/storage.dart';
 
 import '../_local/keys.dart';
 
-Uint8List signMessage(String privKey, Uint8List data) {
-  var key = CryptoUtils.rsaPrivateKeyFromPemPkcs1(privKey);
-  var sign = CryptoUtils.rsaSign(key, data, algorithmName: 'SHA-512/RSA');
-  return sign;
+Uint8List intToBytes(int value) {
+  return Uint8List(8)..buffer.asByteData().setInt64(0, value, Endian.big);
 }
 
 Uint8List hash(Uint8List data) {
   return Digest('SHA-512').process(data);
 }
 
+Uint8List signData(String privKey, Uint8List data) {
+  var key = CryptoUtils.rsaPrivateKeyFromPemPkcs1(privKey);
+  var sign = CryptoUtils.rsaSign(key, data, algorithmName: 'SHA-512/RSA');
+  return sign;
+}
+
+Uint8List signArrayConcatenation(List<dynamic> values) {
+  /// working with strings, bytes and integers
+  Uint8List byteArray = Uint8List.fromList([]);
+  values.forEach((singleListValue) {
+    if (singleListValue is String) {
+      var stringAsBytes = singleListValue.codeUnits;
+      stringAsBytes.forEach((byte) {
+        byteArray.add(byte);
+      });
+    }
+    if (singleListValue is Uint8List) {
+      singleListValue.forEach((byte) {
+        byteArray.add(byte);
+      });
+    }
+    if (singleListValue is int) {
+      Uint8List intAsBytes = intToBytes(singleListValue);
+      intAsBytes.forEach((byte) {
+        byteArray.add(byte);
+      });
+    }
+  });
+  return byteArray;
+}
+
 Future<String> getPersonalAdress() async {
-  var prefs = await SharedPreferences.getInstance();
-  var persPub = prefs.getString('persPub')!;
-  var persPubBytes = keyToBytes(persPub);
+  var value = await loadValue(StorageKey.publicKey);
+  var persPubBytes = keyToBytes(value);
   var adress = hash(persPubBytes);
   return base64.encode(adress);
 }
