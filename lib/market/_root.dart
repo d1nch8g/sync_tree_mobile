21b4ccaaf886cc186a9c71ee885070599002aae2
Danila Fomin <dangdancheg@gmail.com>
2021-09-15 15:22:07 +0300

    added animation when list view is changing on search

--- lib/market/_root.dart
+++ lib/market/_root.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sync_tree_mobile_logic/net/info_calls.dart';
-import 'package:sync_tree_mobile_logic/net/user_calls.dart';
 
 class MarketPage extends StatelessWidget {
   @override
@@ -31,10 +30,12 @@ class _SearchBlockState extends State<SearchBlock> {
 
   updateMarketList() async {
     var marketAdresses = await InfoCalls.searchMarkets(searchController.text);
-    markets.clear();
+    this.markets.clear();
     marketAdresses.forEach((marketAdress) async {
       var info = await InfoCalls.marketInfo(marketAdress);
-      markets.add(info);
+      setState(() {
+        this.markets.add(info);
+      });
     });
   }
 
@@ -49,6 +50,10 @@ class _SearchBlockState extends State<SearchBlock> {
             style: TextStyle(
               color: Theme.of(context).cardColor,
             ),
+            onEditingComplete: () {
+              updateMarketList();
+              FocusScope.of(context).unfocus();
+            },
             decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -73,18 +78,60 @@ class _SearchBlockState extends State<SearchBlock> {
             cursorColor: Theme.of(context).cardColor,
           ),
         ),
-        MarketTileList(markets: markets)
+        AnimatedSwitcher(
+          child: MarketTileList(
+            markets: markets,
+            key: UniqueKey(),
+          ),
+          duration: Duration(milliseconds: 377),
+        ),
       ],
     );
   }
 }
 
 class MarketTileList extends StatelessWidget {
-  List<MarketInfo> markets;
-  MarketTileList({required this.markets});
+  final List<MarketInfo> markets;
+  final Key key;
+  MarketTileList({required this.markets, required this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: markets.length,
+      padding: EdgeInsets.all(3.0),
+      itemBuilder: (context, index) {
+        var mkt = markets[index];
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Image.network(mkt.imageLink),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    mkt.name,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Text(
+                    'count: ' + mkt.operationCount.toString() + '  ',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ],
+              ),
+              subtitle: Text(
+                mkt.description.substring(0, 100) + '...',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Divider(
+              color: Theme.of(context).cardColor,
+            )
+          ],
+        );
+      },
+    );
   }
 }
