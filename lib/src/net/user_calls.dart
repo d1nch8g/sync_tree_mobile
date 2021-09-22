51640b1ad2b805d5f41bf46eb7abc17e47d50ae6
Danila Fomin <dangdancheg@gmail.com>
2021-09-22 13:04:22 +0300

    .

--- lib/src/net/user_calls.dart
+++ lib/src/net/user_calls.dart
@@ -7,10 +7,10 @@ import 'api.pb.dart';
 import 'api.pbgrpc.dart';
 import 'api.dart';
 
-/// # ATTENTION
-/// all methods called by that class can throw connection error
+/// # Those methods are not changing anything in local storage! They only
+/// throw an error in case of network fail operation
 class UserCalls {
-  static Future<bool> create() async {
+  static Future create() async {
     var publicName = await Storage.loadPublicName();
     var keys = await Storage.loadKeys();
     var sign = await keys.personal.private.signList([
@@ -18,40 +18,34 @@ class UserCalls {
       keys.message.public.bytes,
       publicName,
     ]);
-    final response = await stub.userCreate(
-      UserCreateRequest(
+    await userStub.create(
+      UserRequests_Create(
         publicKey: keys.personal.public.bytes,
         messsageKey: keys.message.public.bytes,
         publicName: publicName,
         sign: sign,
       ),
     );
-    return response.passed;
   }
 
-  static Future<bool> updateName(String name) async {
+  static Future update(String name) async {
     var keys = await Storage.loadKeys();
     var sign = await keys.personal.private.signList([
       keys.personal.public.bytes,
       keys.message.public.bytes,
       name,
     ]);
-    final response = await stub.userUpdate(
-      UserUpdateRequest(
+    await userStub.update(
+      UserRequests_Update(
         publicKey: keys.personal.public.bytes,
         messsageKey: keys.message.public.bytes,
         publicName: name,
         sign: sign,
       ),
     );
-    if (response.passed) {
-      Storage.savePublicName(name);
-      return true;
-    }
-    return false;
   }
 
-  static Future<bool> sendMain(int amount, String recieverAdress) async {
+  static Future send(int amount, String recieverAdress) async {
     var keys = await Storage.loadKeys();
     var adressBytes = base64.decode(recieverAdress);
     var sign = await keys.personal.private.signList([
@@ -59,61 +53,17 @@ class UserCalls {
       amount,
       adressBytes,
     ]);
-    final response = await stub.userSend(
-      UserSendRequest(
+    await userStub.send(
+      UserRequests_Send(
         publicKey: keys.personal.public.bytes,
         sendAmount: Int64(amount),
         recieverAdress: adressBytes,
         sign: sign,
       ),
     );
-    if (response.passed) {
-      var curBalance = await Storage.loadMainBalance();
-      Storage.saveMainBalance(curBalance - amount);
-      return true;
-    }
-    return false;
   }
 
-  static Future<bool> deposit(String marketAdress, int amount) async {
-    var keys = await Storage.loadKeys();
-    var bytesMarketAdress = base64.decode(marketAdress);
-    var sign = await keys.personal.private.signList([
-      keys.personal.public.bytes,
-      bytesMarketAdress,
-      amount,
-    ]);
-    final response = await stub.userDeposit(
-      UserDepositRequest(
-        publicKey: keys.personal.public.bytes,
-        marketAdress: bytesMarketAdress,
-        amount: Int64(amount),
-        sign: sign,
-      ),
-    );
-    return response.passed;
-  }
-
-  static Future<bool> withdrawal(String marketAdress, int amount) async {
-    var keys = await Storage.loadKeys();
-    var bytesMarketAdress = base64.decode(marketAdress);
-    var sign = await keys.personal.private.signList([
-      keys.personal.public.bytes,
-      bytesMarketAdress,
-      amount,
-    ]);
-    final response = await stub.userWithdrawal(
-      UserWithdrawalRequest(
-        publicKey: keys.personal.public.bytes,
-        marketAdress: bytesMarketAdress,
-        amount: Int64(amount),
-        sign: sign,
-      ),
-    );
-    return response.passed;
-  }
-
-  static Future<bool> sendMessage(String marketAdress, String message) async {
+  static Future message(String marketAdress, String message) async {
     var keys = await Storage.loadKeys();
     var bytesMarketAdress = base64.decode(marketAdress);
     var sign = await keys.personal.private.signList([
@@ -121,18 +71,17 @@ class UserCalls {
       bytesMarketAdress,
       message,
     ]);
-    final response = await stub.userSendMessage(
-      UserSendMessageRequest(
+    await userStub.message(
+      UserRequests_Message(
         publicKey: keys.personal.public.bytes,
         adress: bytesMarketAdress,
         message: message,
         sign: sign,
       ),
     );
-    return response.passed;
   }
 
-  static Future<bool> buy(String marketAdress, int recieve, int offer) async {
+  static Future buy(String marketAdress, int recieve, int offer) async {
     var keys = await Storage.loadKeys();
     var bytesMarketAdress = base64.decode(marketAdress);
     var sign = await keys.personal.private.signList([
@@ -141,8 +90,8 @@ class UserCalls {
       recieve,
       offer,
     ]);
-    final response = await stub.userBuy(
-      UserBuyRequest(
+    await userStub.buy(
+      UserRequests_Buy(
         publicKey: keys.personal.public.bytes,
         adress: bytesMarketAdress,
         recieve: Int64(recieve),
@@ -150,7 +99,6 @@ class UserCalls {
         sign: sign,
       ),
     );
-    return response.passed;
   }
 
   static Future<bool> sell(String marketAdress, int recieve, int offer) async {
