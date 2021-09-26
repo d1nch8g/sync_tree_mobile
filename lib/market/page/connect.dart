c1ef75fc0fe93023394bfbae1ff8f5086b0f1963
Danila Fomin <dangdancheg@gmail.com>
2021-09-26 13:10:42 +0300

    .

---
+++ lib/market/page/connect.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ConnectButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Text('connect'),
+      onPressed: () {},
+    );
+  }
+}
