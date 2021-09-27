175374a36c017e18bfb399fa1ea9fccaaebe3ecb
Danila Fomin <dangdancheg@gmail.com>
2021-09-28 00:05:28 +0300

    .

--- lib/wallet/page/chatText.dart
+++ lib/wallet/page/chatText.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ChatTextField extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -26,7 +27,7 @@ class ChatTextField extends StatelessWidget {
           labelStyle: TextStyle(
             color: Theme.of(context).focusColor,
           ),
-          labelText: 'type market name',
+          labelText: 'message to market',
           hoverColor: Theme.of(context).focusColor,
           fillColor: Theme.of(context).focusColor,
           focusColor: Theme.of(context).focusColor,
