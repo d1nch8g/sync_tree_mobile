656e1a70bce0f3de54e8f6229796206975bdc25d
Danila Fomin <dangdancheg@gmail.com>
2021-09-29 02:19:01 +0300

    added deposit overlay

--- lib/wallet/page/buttons.dart
+++ lib/wallet/page/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sync_tree_mobile_ui/wallet/page/deposit.dart';
 
 class WalletOperations extends StatelessWidget {
   final Function closeWallet;
@@ -11,15 +12,7 @@ class WalletOperations extends StatelessWidget {
       children: [
         Expanded(
           child: Center(
-            child: TextButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (_) => DepositOverlay(),
-                );
-              },
-              child: Text('deposit'),
-            ),
+            child: DepositButton(),
           ),
         ),
         IconButton(
@@ -40,75 +33,6 @@ class WalletOperations extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class DepositOverlay extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => DepositOverlayState();
-}
-
-class DepositOverlayState extends State<DepositOverlay>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> scaleAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 377),
-    );
-    scaleAnimation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.decelerate,
-    );
-    controller.addListener(() {
-      setState(() {});
-    });
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Material(
-        color: Colors.transparent,
-        child: ScaleTransition(
-          scale: scaleAnimation,
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.80,
-            decoration: ShapeDecoration(
-              color: Theme.of(context).backgroundColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(50, 32, 50, 22),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'RECIEVE',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  Divider(color: Theme.of(context).focusColor),
-                  Text(
-                    'This button will copy your adress to clipboard. Give it'
-                    ' to sender.',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline2,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
---
+++ lib/wallet/page/deposit.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class DepositButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (_) => GetAdressOverlay(),
+        );
+      },
+      child: Text('deposit'),
+    );
+  }
+}
+
+class GetAdressOverlay extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GetAdressOverlayState();
+}
+
+class GetAdressOverlayState extends State<GetAdressOverlay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 377),
+    );
+    scaleAnimation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    );
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.80,
+            decoration: ShapeDecoration(
+              color: Theme.of(context).backgroundColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(32, 22, 32, 22),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'DEPOSIT',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  Divider(color: Theme.of(context).focusColor),
+                  Text(
+                    'Type the amount you want to deposit.',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  SizedBox(height: 4),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
