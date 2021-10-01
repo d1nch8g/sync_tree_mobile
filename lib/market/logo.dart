cb1d56a103ed8eb8460a4523f0f4a32887d3a8b1
Danila Fomin <dangdancheg@gmail.com>
2021-10-01 11:31:12 +0300

    .

--- lib/market/logo.dart
+++ lib/market/logo.dart
@@ -12,17 +12,33 @@ class _SearchLogoState extends State<SearchLogo> {
       children: [
         Icon(
           Icons.stacked_line_chart_rounded,
-          size: MediaQuery.of(context).size.height * 0.15,
+          size: MediaQuery.of(context).size.height * 0.125,
           color: Theme.of(context).hintColor,
         ),
+        SizedBox(width: 8),
         Column(
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Search module',
+              'Market search',
               style: Theme.of(context).textTheme.headline4,
             ),
-            
+            Text(
+              'Found: 8',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'Connected: 2',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'Connected: 2',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headline5,
+            ),
           ],
         ),
       ],
