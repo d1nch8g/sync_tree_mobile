d3d5689e4b08b6ec81b8bfed19318c1edd8aa312
Danila Fomin <dangdancheg@gmail.com>
2021-10-01 15:03:38 +0300

    .

--- lib/market/modal.dart
+++ lib/market/modal.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:sync_tree_mobile_ui/market/page/connect.dart';
 import 'package:sync_tree_mobile_ui/market/page/info.dart';
+import 'package:sync_tree_mobile_ui/src/local/balance.dart';
 import 'package:sync_tree_mobile_ui/src/net/info_calls.dart';
 
 class MarketModalPage extends StatefulWidget {
@@ -11,9 +13,20 @@ class MarketModalPage extends StatefulWidget {
 }
 
 class _MarketModalPageState extends State<MarketModalPage> {
+  String balance = '0';
+
+  updateBalance() async {
+    balance = await Balance.marketBalance(
+      adress: widget.info.adress,
+      delimiter: widget.info.delimiter,
+    );
+    setState(() {});
+  }
+
   @override
   void initState() {
     super.initState();
+    updateBalance();
   }
 
   @override
@@ -31,8 +44,65 @@ class _MarketModalPageState extends State<MarketModalPage> {
         padding: const EdgeInsets.all(18.0),
         child: Column(
           children: [
+            SizedBox(height: 8),
+            Row(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: widget.info.imageLink,
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  width: 48,
+                ),
+                SizedBox(width: 18),
+                Text(
+                  widget.info.name,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+                Spacer(),
+                Text(
+                  balance,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+              ],
+            ),
+            Divider(
+              color: Theme.of(context).focusColor,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Input fee: ${widget.info.inputFee / 100}%\n'
+                    'Buys count: ${widget.info.activeBuys}\n'
+                    'Operations: ${widget.info.operationCount}',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Output fee: ${widget.info.outputFee / 100}%\n'
+                    'Sells count: ${widget.info.activeSells}\n'
+                    '${widget.info.workTime}',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                )
+              ],
+            ),
+            Divider(
+              color: Theme.of(context).focusColor,
+            ),
             Expanded(
-              child: MarketInfoWidget(info: widget.info),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    widget.info.description,
+                    style: Theme.of(context).textTheme.bodyText2,
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
             ),
             ConnectionButton(adress: widget.info.adress)
           ],
