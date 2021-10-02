a3d1d7fd3e36ab7d840cc897a41847f0fc30e45c
Danila Fomin <dangdancheg@gmail.com>
2021-10-02 12:29:44 +0300

    .

--- lib/wallet/page/chat.dart
+++ lib/wallet/page/chat.dart
@@ -7,6 +7,103 @@ import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_4.dart';
 import 'package:sync_tree_mobile_ui/src/local/balance.dart';
 import 'package:sync_tree_mobile_ui/src/local/storage.dart';
 import 'package:sync_tree_mobile_ui/src/net/info_calls.dart';
+import 'dart:typed_data';
+
+import 'package:sync_tree_mobile_ui/src/net/user_calls.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+
+class ChatTextField extends StatelessWidget {
+  final String marketAdress;
+  final Uint8List marketMesKey;
+  final TextEditingController controller = TextEditingController();
+  ChatTextField({
+    required this.marketMesKey,
+    required this.marketAdress,
+  });
+
+  void showNotDeliveredSnackBar(context) {
+    showTopSnackBar(
+      context,
+      CustomSnackBar.error(
+        message: 'Unable to send message!',
+        textStyle: Theme.of(context).textTheme.headline2!,
+        icon: const Icon(
+          Icons.message_rounded,
+          color: const Color(0x15000000),
+          size: 120,
+        ),
+        iconRotationAngle: 8,
+      ),
+    );
+  }
+
+  void sendMessage(context, String message) async {
+    if (message == '') {
+      return;
+    }
+    var delivered = await UserCalls.message(
+      marketAdress: marketAdress,
+      marketMesKey: marketMesKey,
+      message: message,
+    );
+    if (delivered) {
+      Storage.addMessage(
+        message: 'uu' + message,
+        adress: marketAdress,
+      );
+      return;
+    }
+    showNotDeliveredSnackBar(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+          color: Theme.of(context).focusColor,
+        ),
+        cursorColor: Theme.of(context).focusColor,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderSide: BorderSide(color: Theme.of(context).focusColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderSide: BorderSide(color: Theme.of(context).focusColor),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderSide: BorderSide(color: Theme.of(context).focusColor),
+          ),
+          labelStyle: TextStyle(
+            color: Theme.of(context).focusColor,
+          ),
+          labelText: 'message to market',
+          hoverColor: Theme.of(context).focusColor,
+          fillColor: Theme.of(context).focusColor,
+          focusColor: Theme.of(context).focusColor,
+          suffixIcon: IconButton(
+            icon: Icon(Icons.send),
+            color: Theme.of(context).focusColor,
+            onPressed: () {
+              sendMessage(context, controller.text);
+              FocusScope.of(context).unfocus();
+            },
+          ),
+        ),
+        onEditingComplete: () {
+          sendMessage(context, controller.text);
+          FocusScope.of(context).unfocus();
+        },
+      ),
+    );
+  }
+}
 
 class ChatMessages extends StatefulWidget {
   final String adress;
