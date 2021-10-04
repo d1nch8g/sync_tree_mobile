a971e7f5949e5771fcbd4c6ba26cc048f20777a3
Danila Fomin <dangdancheg@gmail.com>
2021-10-04 11:31:27 +0300

    .

--- lib/wallet/page/chat.dart
+++ lib/wallet/page/chat.dart
@@ -276,3 +276,23 @@ class _ChatMessagesState extends State<ChatMessages> {
     );
   }
 }
+
+class CustomBubble extends StatelessWidget {
+  final String text;
+  final Color color;
+  final bool rightSide;
+  CustomBubble({
+    required this.text,
+    required this.color,
+    required this.rightSide,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Container(
+        
+      ),
+    );
+  }
+}
