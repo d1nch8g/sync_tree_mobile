a40edb1b3c112a059818f2ff4b85946695149a78
Danila Fomin <dangdancheg@gmail.com>
2021-10-06 13:28:37 +0300

    .

--- lib/introduction.dart
+++ lib/introduction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import '../src/src.dart';
 
 Future<String> generateKeys(bool start) async {
@@ -15,34 +16,67 @@ class IntroPage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Hello!',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            SizedBox(height: 12),
-            Padding(
-              padding: const EdgeInsets.all(22.0),
-              child: Text(
-                'We are building your private keys, you can pick public name '
-                'that will be displayed to other users.',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline2,
+        child: IntroductionScreen(
+          pages: <PageViewModel>[
+            PageViewModel(
+              title: 'Hello!',
+              image: Icon(
+                Icons.info_rounded,
+                size: MediaQuery.of(context).size.width * 0.40,
+                color: Theme.of(context).focusColor,
+              ),
+              body: 'Here you can get a brief explanation of what'
+                  'is going in this app, and how it works!',
+              decoration: PageDecoration(
+                titleTextStyle: Theme.of(context).textTheme.headline1!,
+                bodyTextStyle: Theme.of(context).textTheme.headline2!,
+              ),
+              footer: TextButton(
+                onPressed: () {},
+                child: Text('next'),
               ),
             ),
-            SizedBox(height: 12),
-            TextButton(
-              child: Text('continue'),
-              onPressed: () async {
-                Navigator.pushNamed(context, '/main');
-                var keys = await compute(generateKeys, true);
-                Storage.saveKeys(keys);
-              },
+            PageViewModel(
+              title: "Title of first page",
+              body:
+                  "Here you can write the description of the page, to explain someting...",
+            ),
+            PageViewModel(
+              title: "Title of first page",
+              body:
+                  "Here you can write the description of the page, to explain someting...",
+            ),
+            PageViewModel(
+              title: "Title of first page",
+              body:
+                  "Here you can write the description of the page, to explain someting...",
+            ),
+            PageViewModel(
+              title: "Title of first page",
+              body:
+                  "Here you can write the description of the page, to explain someting...",
+            ),
+            PageViewModel(
+              title: "Title of first page",
+              body:
+                  "Here you can write the description of the page, to explain someting...",
             ),
           ],
+          animationDuration: 387,
+          showDoneButton: false,
+          showNextButton: false,
+          isTopSafeArea: true,
+          isBottomSafeArea: true,
+          dotsDecorator: DotsDecorator(
+            size: const Size.square(8.0),
+            activeSize: const Size(25.0, 8.0),
+            activeColor: Theme.of(context).focusColor,
+            color: Theme.of(context).focusColor,
+            spacing: const EdgeInsets.symmetric(horizontal: 4.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
       ),
     );
