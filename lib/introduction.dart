41a9ab9c8b42395ee7cde191ced907eb293e68b8
Danila Fomin <dangdancheg@gmail.com>
2021-10-06 14:29:05 +0300

    added a lot of stuff on intro page

--- lib/introduction.dart
+++ lib/introduction.dart
@@ -15,69 +15,181 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(
-        child: IntroductionScreen(
-          pages: <PageViewModel>[
-            PageViewModel(
-              title: 'Hello!',
-              image: Icon(
-                Icons.info_rounded,
-                size: MediaQuery.of(context).size.width * 0.40,
-                color: Theme.of(context).focusColor,
-              ),
-              body: 'Here you can get a brief explanation of what'
-                  'is going in this app, and how it works!',
-              decoration: PageDecoration(
-                titleTextStyle: Theme.of(context).textTheme.headline1!,
-                bodyTextStyle: Theme.of(context).textTheme.headline2!,
-              ),
-              footer: TextButton(
-                onPressed: () {},
-                child: Text('next'),
-              ),
+      body: IntroductionScreen(
+        freeze: true,
+        pages: <PageViewModel>[
+          PageViewModel(
+            title: 'Hello!',
+            image: Icon(
+              Icons.info_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
             ),
-            PageViewModel(
-              title: "Title of first page",
-              body:
-                  "Here you can write the description of the page, to explain someting...",
-            ),
-            PageViewModel(
-              title: "Title of first page",
-              body:
-                  "Here you can write the description of the page, to explain someting...",
-            ),
-            PageViewModel(
-              title: "Title of first page",
-              body:
-                  "Here you can write the description of the page, to explain someting...",
-            ),
-            PageViewModel(
-              title: "Title of first page",
-              body:
-                  "Here you can write the description of the page, to explain someting...",
-            ),
-            PageViewModel(
-              title: "Title of first page",
-              body:
-                  "Here you can write the description of the page, to explain someting...",
-            ),
-          ],
-          animationDuration: 387,
-          showDoneButton: false,
-          showNextButton: false,
-          isTopSafeArea: true,
-          isBottomSafeArea: true,
-          dotsDecorator: DotsDecorator(
-            size: const Size.square(8.0),
-            activeSize: const Size(25.0, 8.0),
-            activeColor: Theme.of(context).focusColor,
-            color: Theme.of(context).focusColor,
-            spacing: const EdgeInsets.symmetric(horizontal: 4.0),
-            activeShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+            body: 'Here you can get a brief explanation of what '
+                'is going on in this app, and how it works.',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
             ),
           ),
+          PageViewModel(
+            title: 'Search',
+            image: Icon(
+              Icons.manage_search_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'You can search for different markets using '
+                'built in search engine.',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Wallets',
+            image: Icon(
+              Icons.account_balance_wallet_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'When you found market suitable for yourself, '
+                'you can connect it as a wallet.',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Balance',
+            image: Icon(
+              Icons.input_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'Each time you process deposit or withdrawal '
+                'operation, you interact directly with that market '
+                'using messages stored in blockchain in encrypted way.',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Trade',
+            image: Icon(
+              Icons.query_stats_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'You can buy or sell any asset using built in '
+                'sync tree token. Fee for any trade operation is 0% '
+                '(yes, it is completely free).',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Security',
+            image: Icon(
+              Icons.shield_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'Only owner of private key can process changes in '
+                'blockchain. Keep your private key in safe and '
+                'secure place.',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Threats',
+            image: Icon(
+              Icons.dangerous_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            body: 'Deceivers can try to create markets that are looking '
+                'similar to original. Double check each market you '
+                'interact with. Do not process any transactions on '
+                'untrusted markets!',
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+              bodyTextStyle: Theme.of(context).textTheme.headline2!,
+            ),
+          ),
+          PageViewModel(
+            title: 'Names',
+            image: Icon(
+              Icons.supervised_user_circle_rounded,
+              size: MediaQuery.of(context).size.width * 0.40,
+              color: Theme.of(context).focusColor,
+            ),
+            bodyWidget: TextField(
+              controller: TextEditingController(),
+              style: TextStyle(
+                color: Theme.of(context).focusColor,
+              ),
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                labelStyle: TextStyle(
+                  color: Theme.of(context).focusColor,
+                ),
+                labelText: 'name',
+                hoverColor: Theme.of(context).focusColor,
+                fillColor: Theme.of(context).focusColor,
+                focusColor: Theme.of(context).focusColor,
+              ),
+              cursorColor: Theme.of(context).focusColor,
+            ),
+            decoration: PageDecoration(
+              titleTextStyle: Theme.of(context).textTheme.headline1!,
+            ),
+            footer: Text(
+              'Each user and market name in this system is '
+              'unique! Name check is a true way of identity verification. '
+              'Pick your\'s.',
+              style: Theme.of(context).textTheme.headline2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+        animationDuration: 387,
+        showNextButton: true,
+        next: Text('next'),
+        nextFlex: 35,
+        showDoneButton: true,
+        done: Text('done'),
+        onDone: () {
+          Navigator.pushNamed(context, '/main');
+        },
+        skipFlex: 0,
+        dotsFlex: 86,
+        dotsDecorator: DotsDecorator(
+          size: const Size.square(8.0),
+          activeSize: const Size(25.0, 8.0),
+          activeColor: Theme.of(context).focusColor,
+          color: Theme.of(context).focusColor,
+          spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
+        controlsPadding: EdgeInsets.all(28),
       ),
     );
   }
--- lib/start/name.dart
+++ lib/start/name.dart
@@ -1,169 +1,169 @@
-// import 'package:flutter/material.dart';
-// import '../src/src.dart';
+import 'package:flutter/material.dart';
+import '../src/src.dart';
 
-// class NameCreation extends StatefulWidget {
-//   @override
-//   _NameCreationState createState() => _NameCreationState();
-// }
+class NameCreation extends StatefulWidget {
+  @override
+  _NameCreationState createState() => _NameCreationState();
+}
 
-// class _NameCreationState extends State<NameCreation> {
-//   final nameController = TextEditingController();
+class _NameCreationState extends State<NameCreation> {
+  final nameController = TextEditingController();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       resizeToAvoidBottomInset: false,
-//       backgroundColor: Theme.of(context).backgroundColor,
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text(
-//             'pick a name',
-//             style: Theme.of(context).textTheme.headline1,
-//           ),
-//           SizedBox(height: 12),
-//           Padding(
-//             padding: const EdgeInsets.all(22.0),
-//             child: Text(
-//               'You can set another one later in the settings.',
-//               textAlign: TextAlign.center,
-//               style: Theme.of(context).textTheme.headline2,
-//             ),
-//           ),
-//           SizedBox(height: 12),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: TextField(
-//               controller: nameController,
-//               style: TextStyle(
-//                 color: Theme.of(context).focusColor,
-//               ),
-//               decoration: InputDecoration(
-//                 enabledBorder: OutlineInputBorder(
-//                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//                   borderSide: BorderSide(color: Theme.of(context).focusColor),
-//                 ),
-//                 focusedBorder: OutlineInputBorder(
-//                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//                   borderSide: BorderSide(color: Theme.of(context).focusColor),
-//                 ),
-//                 border: OutlineInputBorder(
-//                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//                   borderSide: BorderSide(color: Theme.of(context).focusColor),
-//                 ),
-//                 labelStyle: TextStyle(
-//                   color: Theme.of(context).focusColor,
-//                 ),
-//                 labelText: 'name',
-//                 hoverColor: Theme.of(context).focusColor,
-//                 fillColor: Theme.of(context).focusColor,
-//                 focusColor: Theme.of(context).focusColor,
-//               ),
-//               cursorColor: Theme.of(context).focusColor,
-//             ),
-//           ),
-//           SizedBox(height: 23),
-//           TextButton(
-//             child: Text('continue'),
-//             onPressed: () {
-//               var nameCreationError = Filter.operateCheck(nameController.text);
-//               if (nameCreationError == '') {
-//                 Storage.savePublicName(nameController.text);
-//                 Navigator.pushNamed(context, '/keys');
-//               } else {
-//                 showDialog(
-//                   context: context,
-//                   builder: (_) => NameErrorOverlay(
-//                     errorMessage: nameCreationError,
-//                   ),
-//                 );
-//               }
-//             },
-//           ),
-//           SizedBox(height: 125),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'pick a name',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.all(22.0),
+            child: Text(
+              'You can set another one later in the settings.',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ),
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: nameController,
+              style: TextStyle(
+                color: Theme.of(context).focusColor,
+              ),
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Theme.of(context).focusColor),
+                ),
+                labelStyle: TextStyle(
+                  color: Theme.of(context).focusColor,
+                ),
+                labelText: 'name',
+                hoverColor: Theme.of(context).focusColor,
+                fillColor: Theme.of(context).focusColor,
+                focusColor: Theme.of(context).focusColor,
+              ),
+              cursorColor: Theme.of(context).focusColor,
+            ),
+          ),
+          SizedBox(height: 23),
+          TextButton(
+            child: Text('continue'),
+            onPressed: () {
+              var nameCreationError = Filter.operateCheck(nameController.text);
+              if (nameCreationError == '') {
+                Storage.savePublicName(nameController.text);
+                Navigator.pushNamed(context, '/keys');
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (_) => NameErrorOverlay(
+                    errorMessage: nameCreationError,
+                  ),
+                );
+              }
+            },
+          ),
+          SizedBox(height: 125),
+        ],
+      ),
+    );
+  }
+}
 
-// class NameErrorOverlay extends StatefulWidget {
-//   final String errorMessage;
-//   NameErrorOverlay({required this.errorMessage});
-//   @override
-//   State<StatefulWidget> createState() => NameErrorOverlayState();
-// }
+class NameErrorOverlay extends StatefulWidget {
+  final String errorMessage;
+  NameErrorOverlay({required this.errorMessage});
+  @override
+  State<StatefulWidget> createState() => NameErrorOverlayState();
+}
 
-// class NameErrorOverlayState extends State<NameErrorOverlay>
-//     with SingleTickerProviderStateMixin {
-//   late AnimationController controller;
-//   late Animation<double> scaleAnimation;
+class NameErrorOverlayState extends State<NameErrorOverlay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller = AnimationController(
-//       vsync: this,
-//       duration: Duration(milliseconds: 377),
-//     );
-//     scaleAnimation = CurvedAnimation(
-//       parent: controller,
-//       curve: Curves.decelerate,
-//     );
-//     controller.addListener(() {
-//       setState(() {});
-//     });
-//     controller.forward();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 377),
+    );
+    scaleAnimation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    );
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.forward();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Material(
-//         color: Colors.transparent,
-//         child: ScaleTransition(
-//           scale: scaleAnimation,
-//           child: Container(
-//             width: MediaQuery.of(context).size.width * 0.72,
-//             decoration: ShapeDecoration(
-//               color: Theme.of(context).backgroundColor,
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(15.0),
-//               ),
-//             ),
-//             child: Padding(
-//               padding: const EdgeInsets.all(28.0),
-//               child: Column(
-//                 mainAxisSize: MainAxisSize.min,
-//                 children: [
-//                   Icon(
-//                     Icons.error_outline_rounded,
-//                     size: 144,
-//                     color: Theme.of(context).focusColor,
-//                   ),
-//                   Text(
-//                     this.widget.errorMessage,
-//                     textAlign: TextAlign.center,
-//                     style: Theme.of(context).textTheme.headline2,
-//                   ),
-//                   SizedBox(
-//                     height: 10,
-//                   ),
-//                   TextButton(
-//                     child: Text(
-//                       'retry',
-//                       textAlign: TextAlign.center,
-//                     ),
-//                     onPressed: () {
-//                       Navigator.pop(context);
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.72,
+            decoration: ShapeDecoration(
+              color: Theme.of(context).backgroundColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.error_outline_rounded,
+                    size: 144,
+                    color: Theme.of(context).focusColor,
+                  ),
+                  Text(
+                    this.widget.errorMessage,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    child: Text(
+                      'retry',
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
